389ef42e57afe3744549c2e6c636623588113ffb
Reversive <matiaspavan@gmail.com>
2024-08-16 13:02:05 -0300

    feat: location domain model definition

---
+++ lib/layers/domain/models/location.dart
@@ -0,0 +1,13 @@
+final class Location {
+  final String street;
+  final int number;
+  final String city;
+  final String state;
+
+  Location({
+    required this.street,
+    required this.number,
+    required this.city,
+    required this.state,
+  });
+}
