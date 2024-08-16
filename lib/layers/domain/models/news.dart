8347e8671e2850ea09deeacf1bb838fd97141d57
Reversive <matiaspavan@gmail.com>
2024-08-16 02:52:20 -0300

    feat: news domain model definition

---
+++ lib/layers/domain/models/news.dart
@@ -0,0 +1,13 @@
+final class News {
+  final String imgURL;
+  final String name;
+  final String title;
+  final String description;
+
+  News({
+    required this.imgURL,
+    required this.name,
+    required this.title,
+    required this.description,
+  });
+}
