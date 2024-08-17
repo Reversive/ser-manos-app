3f33b7dc65d31e9ca0dea91c987b1af04c87bf46
Reversive <matiaspavan@gmail.com>
2024-08-17 14:17:48 -0300

    refactor: removed default boxdecoration for cards

--- lib/layers/presentation/tokens/decorations/bd_card.dart
+++ lib/layers/presentation/tokens/decorations/bd_card.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ser_manos/layers/presentation/tokens/decorations/bd_base.dart';
-import 'package:ser_manos/layers/presentation/tokens/shadows.dart';
 
 final class BDCard extends BDBase {
   BDCard({
     super.color,
     double radius = 2.0,
+    super.boxShadow,
     super.border,
   }) : super(
-          boxShadow: SMShadows.shadow2,
           borderRadius: BorderRadius.circular(radius),
         );
 }
