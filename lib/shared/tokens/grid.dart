fa3f953b333be1e752b221f183f9288ce61a8cfb
Reversive <matiaspavan@gmail.com>
2024-08-24 21:41:45 -0300

    feat: added grid implementation

---
+++ lib/shared/tokens/grid.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Grid extends Padding {
+  const Grid({
+    super.key,
+  }) : super(
+          padding: const EdgeInsets.only(
+            left: 16,
+          ),
+        );
+}
