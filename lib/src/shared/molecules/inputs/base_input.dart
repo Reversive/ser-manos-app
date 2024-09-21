3bf0a18edd22a7ca508bfa1b6fec84ebf04d67b8
Reversive <matiaspavan@gmail.com>
2024-09-21 18:20:12 -0300

    refac: base input now uses widget hook

--- lib/src/shared/molecules/inputs/base_input.dart
+++ lib/src/shared/molecules/inputs/base_input.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ser_manos/src/shared/atoms/icon.dart';
 import 'package:ser_manos/src/core/theme/colors.dart';
 import 'package:ser_manos/src/shared/molecules/inputs/decorations/base_input_decoration.dart';
 import 'package:ser_manos/src/shared/tokens/typography.dart';
 
-class BaseInput extends StatefulWidget {
+class BaseInput extends HookWidget {
   final TextEditingController controller;
   final String? helperText;
   final Widget? suffixIcon;
@@ -42,52 +43,73 @@ class BaseInput extends StatefulWidget {
   });
 
   @override
-  State<BaseInput> createState() => _BaseInputState();
-}
-
-class _BaseInputState extends State<BaseInput> {
-  FocusNode _focusNode = FocusNode();
-  bool _validationFailed = false;
-
-  void _setState() {
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    _focusNode = widget.focusNode ?? FocusNode();
-    super.initState();
-    _focusNode.addListener(_setState);
-  }
-
-  @override
-  void dispose() {
-    _focusNode.removeListener(_setState);
-    //widget.controller.dispose(); // Uncomment this line to dispose the controller
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final focusNode = useFocusNode();
+    final validationFailed = useState(false);
+
+    void onChanged(value) {
+      validationFailed.value = validator(value) != null;
+    }
+
+    IconButton? _buildSuffixIcon(FocusNode focusNode) {
+      if (suffixIcon == null) {
+        return null;
+      }
+
+      if (controller.text.isNotEmpty &&
+          !focusNode.hasFocus &&
+          !permaShowSuffixIcon) {
+        return null;
+      }
+
+      return permaShowSuffixIcon
+          ? IconButton(
+              onPressed: () async {
+                if (onSuffixIconPressed != null) {
+                  await onSuffixIconPressed!();
+                }
+                if (shouldLoseFocusOnSuffixIconPressed) {
+                  focusNode.unfocus();
+                }
+                onChanged(controller.text);
+              },
+              icon: suffixIcon!,
+            )
+          : controller.text.isNotEmpty
+              ? IconButton(
+                  onPressed: () async {
+                    if (onSuffixIconPressed != null) {
+                      await onSuffixIconPressed!();
+                    }
+                    if (shouldLoseFocusOnSuffixIconPressed) {
+                      focusNode.unfocus();
+                    }
+                    onChanged(controller.text);
+                  },
+                  icon: suffixIcon!,
+                )
+              : null;
+    }
+
     return TextFormField(
       style: SMTypography.subtitle01Style(),
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      decoration: widget.decoration ??
+      decoration: decoration ??
           BaseInputDecoration(
-            hintText: widget.hintText,
-            labelText: widget.labelText,
-            helperText: _focusNode.hasFocus ? widget.helperText : null,
-            suffixIcon: _validationFailed
+            hintText: hintText,
+            labelText: labelText,
+            helperText: focusNode.hasFocus ? helperText : null,
+            suffixIcon: validationFailed.value
                 ? SMIcon.error()
-                : _buildSuffixIcon(_focusNode),
-            floatingLabelBehavior: widget.hintText != null
+                : _buildSuffixIcon(focusNode),
+            floatingLabelBehavior: hintText != null
                 ? FloatingLabelBehavior.always
                 : FloatingLabelBehavior.auto,
-            labelStyle: _validationFailed
+            labelStyle: validationFailed.value
                 ? SMTypography.captionStyle(
                     color: SMColors.error100,
                   )
-                : _focusNode.hasFocus
+                : focusNode.hasFocus
                     ? SMTypography.captionStyle(
                         color: SMColors.secondary200,
                       )
@@ -95,60 +117,14 @@ class _BaseInputState extends State<BaseInput> {
                         color: SMColors.neutral75,
                       ),
           ),
-      controller: widget.controller,
-      enabled: widget.enabled,
-      focusNode: _focusNode,
-      onChanged: _onChanged,
-      validator: (value) => widget.validator(value),
-      keyboardType: widget.keyboardType,
-      inputFormatters: widget.inputFormatters,
-      obscureText: widget.obscureText,
+      controller: controller,
+      enabled: enabled,
+      focusNode: focusNode,
+      onChanged: onChanged,
+      validator: (value) => validator(value),
+      keyboardType: keyboardType,
+      inputFormatters: inputFormatters,
+      obscureText: obscureText,
     );
-  }
-
-  void _onChanged(value) {
-    setState(() {
-      _validationFailed = widget.validator(value) != null;
-    });
-  }
-
-  IconButton? _buildSuffixIcon(FocusNode focusNode) {
-    if (widget.suffixIcon == null) {
-      return null;
-    }
-
-    if (widget.controller.text.isNotEmpty &&
-        !focusNode.hasFocus &&
-        !widget.permaShowSuffixIcon) {
-      return null;
-    }
-
-    return widget.permaShowSuffixIcon
-        ? IconButton(
-            onPressed: () async {
-              if (widget.onSuffixIconPressed != null) {
-                await widget.onSuffixIconPressed!();
-              }
-              if (widget.shouldLoseFocusOnSuffixIconPressed) {
-                focusNode.unfocus();
-              }
-              _onChanged(widget.controller.text);
-            },
-            icon: widget.suffixIcon!,
-          )
-        : widget.controller.text.isNotEmpty
-            ? IconButton(
-                onPressed: () async {
-                  if (widget.onSuffixIconPressed != null) {
-                    await widget.onSuffixIconPressed!();
-                  }
-                  if (widget.shouldLoseFocusOnSuffixIconPressed) {
-                    focusNode.unfocus();
-                  }
-                  _onChanged(widget.controller.text);
-                },
-                icon: widget.suffixIcon!,
-              )
-            : null;
   }
 }
